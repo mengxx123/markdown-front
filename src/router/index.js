6218c711d4af6195f76fe97fd38391f77468ff98
cjh <1418503647@qq.com>
2018-04-22 17:21:49 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Markdown2Html = resolve => require(['@/views/Markdown2Html'], resolve)
+const Html2Markdown = resolve => require(['@/views/Html2Markdown'], resolve)
+const About = resolve => require(['@/views/About'], resolve)
+
+const Error404 = resolve => require(['@/views/error/Error404'], resolve)
+
+Vue.use(Router)
+
+let routes = [
+    {
+        path: '/',
+        component: Markdown2Html
+    },
+    {
+        path: '/markdown2html',
+        component: Markdown2Html
+    },
+    {
+        path: '/html2markdown',
+        component: Html2Markdown
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '*',
+        component: Error404
+    }
+]
+
+let router = new Router({
+    mode: 'history',
+    routes: routes,
+    scrollBehavior (to, from, savedPosition) {
+        return {
+            x: 0,
+            y: 0
+        }
+    }
+})
+
+export default router
---
+++ src/views/Html2Markdown.vue
@@ -0,0 +1,82 @@
+<template>
+    <my-page title="HTML 转 Markdown">
+        <textarea class="textarea" v-model="result" rows="10" placeholder="HTML"></textarea>
+        <br>
+        <ui-raised-button class="btn" primary label="转换" @click="html2md" />
+        <br>
+        <textarea class="textarea" v-model="code" rows="10" placeholder="Markdown" v-if="code"></textarea>
+    </my-page>
+</template>
+
+<script>
+    const toMarkdown = window.toMarkdown
+
+    export default {
+        data () {
+            return {
+                code: '',
+                result: '',
+                page: {
+                    menu: [
+                        {
+                            type: 'icon',
+                            icon: 'help',
+                            to: '/help'
+                        }
+                    ]
+                }
+            }
+        },
+        mounted() {
+            this.init()
+        },
+        init() {
+            this.initWebIntents()
+        },
+        initWebIntents() {
+            if (!window.intent) {
+                return
+            }
+            let data = window.intent.data
+            this.result = data
+        },
+        methods: {
+            html2md() {
+                var text = this.result
+                if (!text) {
+                    alert('请填写要解码的内容')
+                    return
+                }
+                this.code = toMarkdown(text, {})
+                if (!this.isAddMenu) {
+                    this.isAddMenu = true
+                    this.page.menu.push({
+                        type: 'icon',
+                        icon: 'check',
+                        click: this.finish,
+                        title: '完成'
+                    })
+                }
+            },
+            finish() {
+                window.intent.postResult(this.code)
+                setTimeout(() => {
+                    let owner = window.opener || window.parent
+                    owner.window.close()
+                }, 100)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.textarea {
+    width: 100%;
+    max-width: 400px;
+    padding: 16px;
+    margin-bottom: 16px;
+}
+.btn {
+    margin-bottom: 16px;
+}
+</style>
